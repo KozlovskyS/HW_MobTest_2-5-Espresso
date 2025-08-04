ec23585956b6f81ba206a8530418e80fd28a4f3f
KozlovskyS <skozmail@gmail.com>
2025-08-04 19:55:37 +0300

    initial commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/SimpleAppEspressoTest.java
@@ -0,0 +1,58 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.app.Activity;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@LargeTest
+@RunWith(AllureAndroidJUnit4.class)
+
+public class SimpleAppEspressoTest {
+    //   @Rule
+//    public ActivityTestRule<MainActivity> activityTestRule =
+//            new ActivityTestRule<>(MainActivity.class);
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mainActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+    String textView = "This is home fragment";
+
+    @Test
+    public void testName() {
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+        mainText.check(
+                matches(
+                        withText(textView)
+                )
+        );
+    }
+
+    @Test
+    public void testNameError() {
+        ViewInteraction mainText = onView(
+                withId(R.id.text_home)
+        );
+        mainText.check(
+                matches(
+                        withText("This is home fragments")
+                )
+        );
+    }
+}
